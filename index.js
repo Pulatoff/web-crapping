d0468e592cd9740cb2578baa78872554c9f29bea
Pulatoff <niyozbekpulatov@gmail.com>
2022-06-20 19:40:06 +0500

    saved to DB

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const cheerio = require("cheerio");
 const axios = require("axios");
 const mongoose = require("mongoose");
+const models = require("./model/malModel");
+const fs = require("fs");
 const DB = process.env.DB.replace("<password>", process.env.PASSWORD);
 
 mongoose.connect(
@@ -19,36 +21,46 @@ mongoose.connect(
 );
 
 const url = "https://kun.uz/news/category/uzbekiston";
+let dataObj;
+const results = [];
 
-axios(url).then((response) => {
-  const html = response.data;
-  const $ = cheerio.load(html);
-  const results = [];
-  let a = 0;
-  let b = 0;
-  $(".news", html).each(function () {
-    const news__img = $(this).find("img").attr("src");
-    const titleUrl = $(this).find(".news__title").attr("href");
-    const urlcha = "https://kun.uz" + titleUrl;
-    const title = $(this).find(".news__title").text();
-    const dataObj = {
-      id: a,
-      newImg: news__img,
-      title: title,
-      newsUrl: url,
-      newsParagraph: [],
-    };
-    a++;
-    axios(urlcha).then((res) => {
+async function get() {
+  const array = await axios(url).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    let a = 0;
+    let b = 0;
+    $(".news", html).each(function () {
+      const news__img = $(this).find("img").attr("src");
+      const titleUrl = $(this).find(".news__title").attr("href");
+      const urlcha = "https://kun.uz" + titleUrl;
+      const title = $(this).find(".news__title").text();
+      dataObj = {
+        id: a,
+        newImg: news__img,
+        title: title,
+        newsUrl: urlcha,
+        newsParagraph: [],
+      };
+      a++;
+      results.push(dataObj);
+    });
+    return results;
+  });
+
+  array.forEach((element, index) => {
+    axios(element.newsUrl).then((res) => {
       const html1 = res.data;
       const $$ = cheerio.load(html1);
       $$(".single-content").each(function () {
         const datas = $$(this).find("p").text();
-        dataObj.newsParagraph.push(datas);
+        element.newsParagraph.push(datas);
       });
     });
   });
-});
+  console.log(array);
+}
+get();
 
 app.listen(PORT, () => {
   console.log(`server works on ${PORT} port`);
--- model/malModel.js
+++ model/malModel.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  newImg: {
+    type: String,
+    required: true,
+    title: {
+      type: String,
+      required: true,
+    },
+    newsUrl: {
+      type: String,
+      required: true,
+    },
+    newsParagraph: [String],
+  },
+});
+
+module.exports = mongoose.model("uzbekisnon", schema);
